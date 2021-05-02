f32dcd740a6ba4f658421dea2e74bc18ce869dca
Lucas Gianezi <lucas.gianezi.gomes@gmail.com>
2021-05-02 00:32:09 -0300

    ajustes

---
+++ ex070.py
@@ -0,0 +1,10 @@
+# print layout
+# inicio de variaveis (com valor zero) antes de entrar no while
+# soma dos preços, qtty de $ mais de 1000, prod mais barato
+# while true
+# variavel recebe nome do produto
+# variavel para receber preço
+# variavel de decisão com while para quem digitar errado
+# se decisão não, break e sai do while
+# print dos 3 itens
+
