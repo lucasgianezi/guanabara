15bd3dcd581819dd63e0b9a9418fb8d6a2dbec31
Lucas Gianezi <lucas.gianezi.gomes@gmail.com>
2021-05-03 21:45:21 -0300

    ajustes

--- ex071.py
+++ ex071.py
@@ -1,4 +1,17 @@
-
+# 1) DONE print layout
+# 2) DONE iniciar variaveis valor (recebe inteiro do usuario), restante (inicia com valor) e ced (inicia com 50)
+# 3) DONE inicia while com repetição infinita (while True)
+# 4) DONE faz um if com a condição sendo: se valor restante é maior ou igual a cedula em questão. Então podemos usar no minimo 1 daquela cedula.
+# 5) DONE ai ele calcula quantas cedulas podemos usar: total de cedulas = restante // ced
+# 6) DONE é feito um print com a quantidade de cedulas em questao
+# 7) DONE calculamos o valor restante como restante atual = restante anterior - (total de cedulas)*(valor da cedula)
+# 8) DONE fazemos um if com: se restante é igual a zero, então não há nenhuma cedula mais para usar. e aí fazemos um break!
+# 9) DONE tudo que foi feito entreos itens 4 e 8 está dentro do if cuja condição é de se o valor restante é maior que o valor da cedula em questão
+# 10) DONE caso não seja vamos diminuir o valor da celula até que encontremos uma cedula cujo valor seja menor que o valor restante
+# 11) DONE Fazemos isso colocando dentro de um else varias condições que diminuem o valor da cedula em questão para o valor imediatamente inferior
+# 12) DONE ai ele sai do else, o loop acaba e ele volta para o inicio do while
+# 13) DONE não é preciso colocar nada depois do ced == 1 pois quando essa condição for satisfeita, necessariamente o break será acionado
+# 13) DONE print layout final
 
 print('\n')
 print('='*40)
@@ -10,7 +23,7 @@ restante = valor
 ced = 50
 
 while True:
-    if restante // ced >= 1:
+    if restante >= ced:
         totced = restante//ced
         print(f'Total de {totced} cédulas de R${ced},00.')
         restante -= totced * ced
