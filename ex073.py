8cb3897be3a54cba8ddc64755bd03e8e833f0035
Lucas Gianezi <lucas.gianezi.gomes@gmail.com>
2021-05-16 22:35:48 -0300

    ajustes

--- ex073.py
+++ ex073.py
@@ -1,5 +1,18 @@
-# faz tupla com lista de times
+# DONE faz tupla com lista de times
+# DONE print com os 5 primeiros usando fatiamento
+# DONE print com os 4 ultimos usando fatiamento
+# DONE print dos times em ordem alfabetica usando sorted
+# DONE print da posição da Chapecoense usando sorted + 1
 
 times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio', 'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense', 'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia', 'São Paulo', 'Fluminense', 'Sport Recife', 'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta', 'Atlético-GO')
 
-print(len(times))
+print('-='*20)
+print(f'Lista de times do Brasileirão: {times}')
+print('-='*20)
+print(f'Os 5 primeiros são {times[:5]}')
+print('-='*20)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-='*20)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-='*20)
+print(f'A Chapecoense está na {times.index("Chapecoense")+1}ª posição')
