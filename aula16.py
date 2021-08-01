c4459e074d17fd43ac4ceee41d4298396509545b
Lucas Gianezi <lucas.gianezi.gomes@gmail.com>
2021-08-01 20:20:21 -0300

    ajustes

--- aula16.py
+++ aula16.py
@@ -1,11 +1,13 @@
-portfolio = ('fx', 'rates', 'equities', 'volatility')
+#portfolio = ('fx', 'rates', 'equities', 'volatility')
+portfolio = 'fx'
+portfolio.replace('f','r')
 print(portfolio)
 
 '''
 Em Python existem 3 tipos de variáveis compostas: Tuplas, listas e dicionarios.
 
 Uma variável quando é declarada ela vira um espaço na memória.
-Eu não preciso atribuir. Só de declarar (declarar é escrever "variável =") já vira um espaço na memória.
+Eu não preciso atribuir. Só de declarar (declarar é escrever "variável =", "=" é o sinal de atribuição) já vira um espaço na memória.
 Toda variável simples é um espaço na memória. Somente 1 espaço.
 Quando eu faço a atribuição, o espaço criado na memória quando declaramos a variável é preenchido com o item atribuido.
 
@@ -23,10 +25,10 @@ Tuplas, listas e dicionários.
 
 Ok. Se temos uma variável com varios elementos. Como posso acessar os elementos dela?
 Pq no caso das variáveis simples, a variável é o proprio elemento. Se eu estou me referindo a variavel, eu estou me referindo ao elemento que eu atribui a ela.
-No caso de uma variável composta eu tenho varios elementos. Se eu me referir a variavel eu estarei me referindo ao conjunto de elementos. Então só poderia usar funções que tratassem do conjunto.
+No caso de uma variável composta (seja ela, tupla, lista, dicio e string) eu tenho varios elementos. Se eu me referir a variavel eu estarei me referindo ao conjunto de elementos. Então só poderia usar funções que tratassem do conjunto.
 Mas é possível eu acessar algum elemento específico do conjunto? Sim, é possível.
 
-Os elementos são acessados por indices. Os indices são numéricos e começam no zero.
+Os elementos de varivaeis compostas (tuplas, listas, dicionarios, strings) são acessados por indices. Os indices são numéricos e começam no zero.
 
 se você se referir a variavel, estará se referindo a todos os elementos, a variavel é o conjunto de todos os elementos.
 Os elementos individuais devem ser acessados por indice.Esses indices vão montar a variavel composta.
@@ -36,5 +38,49 @@ Na verdade, strings são variáveis compostas. Strings não são tuplas, strings
 Podemos também fazer fatiamento de tuplas igual fizemos de strings.
 
 Na verdade, essas funções de acessar indices/fatiamento não são propriedades de tuplas e string. São propriedades das variáveis compostas no geral. Como strings e tuplas são variaveis compostas, nós podemos utilizar estes métodos.
+Outras funções de variáveis compostas que podem ser usadas com tuplas: len, estrutura de repetição for, 
+
+Uma característica importante das tuplas é o fato de elas serem IMUTÁVEIS. Depois de declarar uma tuplas, eu não posso atribuir coisas aos elementos dela. Eu não posso substituir os elementos, não posso aumentar a quantidade de elementos, não posso diminuir, ... Para fazer isso seria preciso criar uma nova tupla.
+Se você tentar atribuir um valor a um elemento receberá o erro -> "TypeError: 'tuple' object does not support item assignment"
+(O Replace é um método que nem poderíamos usar pois é um método das string] 
+
+As tuplas nós colocamos entre parenteses () ou sem (se vc deixar sem parenteses ele vai considerar uma tupla)
+As listas nós colocamos entre colchetes []
+Os dicionários nós colocamos entre chaves {}
+
+Os elementos destas estruturas nós separamos por virgula
+
+
+
+
+Formas de usar estrutura de repetição:
+
+lanche = ('Hamburguer','suco','bata frita','pizza','pudim','batata frita')
+
+for cont in range(0, len(lanche)):
+    print('Eu vou comer {lanche[cont]})
+
+for c in lanche:
+    print('Eu vou comer {c})
+
+
+Formas de usar estrutura de repetição (COM POSIÇÃO):
+
+for cont in range(0, len(lanche)):
+    print('Eu vou comer {lanche[cont]} na posição {cont})
+
+for pos, comida in enumerate(lanche):
+    print('Eu vou comer {comida} na posição {pos})
+
+
+
+
+
+
+
+
+
+
+
 
 '''
